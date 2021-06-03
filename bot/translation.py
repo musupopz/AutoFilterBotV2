f089d26385b4d699424c8e05b5f3cbd54f23b705
musupopz <84618336+musupopz@users.noreply.github.com>
2021-06-03 19:53:55 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -54,5 +54,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/musupopz/AutoFilterBotV2">Click Me</a></i>
 """
